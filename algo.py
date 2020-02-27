29a55f5d053a17d40a1d768490d1b6bf540cbb1a
lucasdubos <lucasdubos@gmail.com>
2020-02-27 21:06:02 +0100

    update nouveau a_star sur 0

--- algo.py
+++ algo.py
@@ -4,6 +4,9 @@ from moves import *
 import numpy as np
 import copy
 
+#record brute force 22 coups
+#record xavier 24 coups
+
 
 #finir fonction vacante + relire le code du debut 
 
@@ -12,74 +15,70 @@ def format_where(ret):
 
 
 class node:
-    def __init__(self, nb, dest, old_current_state, parent_node, heuristic):
-        print("on test la valeur :", old_current_state[nb])
-        print("ETAT SANS MOVES\n", old_current_state)
-        if (parent_node != None):
-            print("meme adresse partout\n", parent_node)
+    def __init__(self, final_state, current_state, parent_node, nb, heuristic):
+        # if parent_node != None:
+        #     print("on test la valeur :", parent_node.current_state[nb])
         self.heuristic = heuristic
-        self.pos_zero = format_where(np.where(old_current_state == 0))
-        self.nb = nb # coordonnee reformatee
+        self.old_nb = copy.copy(nb)
+        self.nb = nb
+        # self.value = current_state[nb]
         self.parent_node = parent_node # class du node parent
-        self.zero_moves = "" #  top : 1 , down : 2, right : 3, left : 4
+        if parent_node == None:
+            self.zero_moves = ""
+        else:
+            self.zero_moves = copy.copy(parent_node.zero_moves)
         if parent_node != None:
-            self.current_state = self.new_current(old_current_state, nb)
+            self.current_state = self.moving_nb_to_dest(format_where(np.where(parent_node.current_state == 0)), parent_node.current_state, self.nb)
         else:
-            self.current_state = old_current_state
-        self.cost_value = self.calcul_heuristic(dest)
-        self.count_zero_moves = len(self.zero_moves)
-        print("ETAT FINAL AVEC MOVES\n", self.current_state)
-        print("CHAINE DE CARACTERE IMPORTANTE",self.zero_moves) 
+            self.current_state = current_state
+        self.cost_value = self.calcul_heuristic(self.current_state, final_state)
+        # print("COST",self.cost_value)
 
+        #format_where(np.where(final_state == self.current_state[self.nb]
 
-    def moving_nb_to_dest(self, old_current_state, dest):
-        print("KK", old_current_state[self.pos_zero[0] - 1][self.pos_zero[1]])
-        print("JJ", old_current_state[self.nb])
-
-        if self.pos_zero[0] > dest[0] and old_current_state[self.pos_zero[0] - 1][self.pos_zero[1]] != old_current_state[self.nb]:
-            old_current_state = move_top(old_current_state, self.pos_zero)
-            self.zero_moves += "1"
-        elif self.pos_zero[0] < dest[0] and old_current_state[self.pos_zero[0] + 1][self.pos_zero[1]] != old_current_state[self.nb]:
-            old_current_state = move_bottom(old_current_state, self.pos_zero)
-            self.zero_moves += "2"
-        elif self.pos_zero[1] < dest[1] and old_current_state[self.pos_zero[0]][self.pos_zero[1] + 1] != old_current_state[self.nb]:
-            old_current_state = move_right(old_current_state, self.pos_zero)
-            self.zero_moves += "3"
-        elif self.pos_zero[1] > dest[1] and old_current_state[self.pos_zero[0]][self.pos_zero[1] - 1] != old_current_state[self.nb]:
-            old_current_state = move_left(old_current_state, self.pos_zero)
-            self.zero_moves += "4"
-        else:
-            return False
-        return old_current_state
-
-    def new_current(self, old_current_state, dest): # ramene 0 sur nb qu on est entrain de tester A OPTIMISER cette fonction c'est du troll pck il pete tout
-        dist = 1
-        tmp = copy.deepcopy(old_current_state)
-        while dist != 0:
-            tmp = self.moving_nb_to_dest(tmp, dest)
-            if type(tmp) == bool:
-                print("regarder ici car probleme de mouvement")
-                return False
-            self.pos_zero = format_where(np.where(tmp == 0))
-            dist = abs(self.pos_zero[0] - dest[0]) + abs(self.pos_zero[1] - dest[1])
-            
-        tmp = self.moving_nb_to_dest(tmp, self.nb) #swap final
-        # self.pos_zero = format_where(np.where(old_current_state == 0))
+    def moving_nb_to_dest(self, pos_zero, old_current_state, dest): # a reprendre (fera 1 seul swap)
+        tmp = copy.copy(old_current_state)
+        if pos_zero[0] > dest[0]:
+            tmp = move_top(tmp, pos_zero)
+            self.zero_moves += "t"
+        elif pos_zero[0] < dest[0]:
+            tmp = move_bottom(tmp, pos_zero)
+            self.zero_moves += "d"
+        elif pos_zero[1] < dest[1]:
+            tmp = move_right(tmp, pos_zero)
+            self.zero_moves += "r"
+        elif pos_zero[1] > dest[1]:
+            tmp = move_left(tmp, pos_zero)
+            self.zero_moves += "l"
+        self.nb = pos_zero
+        # print("on a fait un move")
+        # print("etat tmp\n", tmp)
+        # print("le mouvement fait", self.zero_moves)
         return tmp
 
+    def calcul_heuristic(self, current, final): # + real dist   # f = g + h
+        # print("value", self.current_state[self.nb])
+        # print("coordonner nb", self.nb)
 
-    def calcul_heuristic(self, dest): # + real dist   # f = g + h
+        # print("coordonner dest", dest )
+        # print(self.current_state)
+        dist = 0
         if self.heuristic == "plouf": # a changer
             pass
         else:
             # d(A,B)= |X(b) - X(a)| + |Y(b) - Y(a)| distance de a à b -> difference abscisse (valeur abs) + difference ordonnée
-            dist = abs(self.nb[0] - dest[0]) + abs(self.nb[1] - dest[1])
+            for nb in np.nditer(current):
+                tmp1, tmp2 = format_where(np.where(final == nb)) , format_where(np.where(current == nb))
+                dist += abs(tmp1[0] - tmp2[0]) + abs(tmp1[1] - tmp2[1])
+            dist = format_where(np.where(final == self.current_state[self.nb]))
+            dist = abs(self.nb[0] - dist[0]) + abs(self.nb[1] - dist[1])
+
+        # print("heuristic",dist)
+        # print("resultat cost", dist + len(self.zero_moves))
         return dist + len(self.zero_moves)
 
 class algorithme:
     def __init__(self, puzzle, heuristic):
-        self.current_state = puzzle.start
-        self.nb_move = 0 # remplis
         self.nb_states = 0 # remplis
         self.max_nb_state = 0 # remplis
         self.heuristic = heuristic
@@ -88,76 +87,169 @@ class algorithme:
 
     def find_node(self, node):
         lst = []
-        if node.nb[0] + 1 <= self.dim - 1 and (node.parent_node == None or node.nb != node.parent_node.nb):
-            lst.append((node.nb[0] + 1, node.nb[1]))
-        if node.nb[0] - 1 >= 0 and (node.parent_node == None or node.nb != node.parent_node.nb):
-            lst.append((node.nb[0] - 1, node.nb[1]))
-        if node.nb[1] + 1 <= self.dim - 1 and (node.parent_node == None or node.nb != node.parent_node.nb):
-            lst.append((node.nb[0], node.nb[1] + 1))
-        if node.nb[1] - 1 >= 0 and (node.parent_node == None or node.nb != node.parent_node.nb):
-            lst.append((node.nb[0], node.nb[1] - 1))
+        # print("\nFONCTION FIND NODE")
+        # print("node nb :", node.old_nb)
+        # if (node.parent_node != None):
+        #     print("node parent nb", node.old_nb)
+        # print("node.current_state\n",node.current_state)
+
+        if node.old_nb[0] + 1 <= self.dim - 1 and (node.parent_node == None or node.old_nb[0] + 1 != node.nb[0]):
+            lst.append((node.old_nb[0] + 1, node.old_nb[1]))
+        if node.old_nb[0] - 1 >= 0 and (node.parent_node == None or node.old_nb[0] -1 != node.nb[0]):
+            lst.append((node.old_nb[0] - 1, node.old_nb[1]))
+        if node.old_nb[1] + 1 <= self.dim - 1 and (node.parent_node == None or node.old_nb[1] + 1 != node.nb[1]):
+            lst.append((node.old_nb[0], node.old_nb[1] + 1))
+        if node.old_nb[1] - 1 >= 0 and (node.parent_node == None or node.old_nb[1] -1 != node.nb[1]):
+            lst.append((node.old_nb[0], node.old_nb[1] - 1))
+        # print("lst des nouvelles entrees dans open", lst)
         return lst
 
-    def to_opened(self, lst_coord_new_node, parent_node, dest): # rentre les nouveaux noeud dans la liste open
+    def to_opened(self,final_state, lst_coord_new_node, parent_node): # rentre les nouveaux noeud dans la liste open
         for coord in lst_coord_new_node: # NE PAS RENTRER TOUT LES NODES ? + TRIER
-            tmp = node(coord, dest, parent_node.current_state, parent_node, self.heuristic)
-            if tmp.current_state != 0:
-                self.opened.append(tmp)
-        exit()
-        self.opened = sorted(self.opened, key=lambda node: node.count_zero_moves)
+            flag = 2
+            tmp = node(final_state, parent_node.current_state, parent_node, coord, self.heuristic)
+            i = 0
+            for elem in self.opened:
+                if np.array_equal(elem.current_state, tmp.current_state):
+                    if elem.cost_value < tmp.cost_value:
+                        flag = 0
+                    else:
+                        print(len(self.opened))
+                        self.opened.remove(elem)
+                        print(len(self.opened))
+                        flag = 1
+                    # self.opened.remove(elem)
+                    flag = 1
+            if flag != 1 and len(self.closed) > 0:
+                for elem in self.closed:
+                    if np.array_equal(elem.current_state, tmp.current_state):
+                        flag = 0 #remetre 0
+                        if elem.cost_value > tmp.cost_value:
+                            print("le cas casse couille arrive, il faut dont surement supprimer tout les enfants de l elem qui sont dans open - WARNING (dans algo.py fonction to_opened)")
+                            exit()
+            i = 0
+            while i < len(self.opened) and tmp.cost_value > self.opened[i].cost_value:
+                i += 1
+            if flag > 0:
+                tmp_un = len(self.opened)
+                self.opened.insert(i, tmp)
+                tmp_deux = len(self.opened)
+                if (tmp_deux == tmp_un):
+                    exit()
 
-        exit()
-        
-
-    def a_star(self, nb, dest):
-        self.opened = [node(nb, dest, self.current_state, None, self.heuristic)]
+    def a_star(self, puzzle):
+        self.opened = [node(puzzle.target, puzzle.start, None, format_where(np.where(puzzle.start == 0)), self.heuristic)] 
         self.closed = []
-
-        while (self.opened[0].nb != dest): #opened vide ?
+        i = 0
+        while (len(self.closed) == 0 or not np.array_equal(self.closed[-1].current_state , puzzle.target)):
+            if i == 2000:
+                break
+            # print(i)
+            i += 1
+            print(i)
             tmp_nodes = self.find_node(self.opened[0]) # les differents noeuds autour du noeud en cours + pas mettre noeud en cours dans opened or closed (list de coordonnées)
-            self.to_opened(tmp_nodes, self.opened[0], dest) # append node in opened list () + tri list + changer ETAT pour chaque noeud
+            self.to_opened(puzzle.target, tmp_nodes, self.opened[0]) # append node in opened list () + tri list + changer ETAT pour chaque noeud
 # + interchangement
 # + cas particulier relou de xavier le noob
+            # print(self.opened[0].current_state)
             self.closed.append(self.opened[0])
+            del self.opened[0]
+            # for noeud in self.opened:
+            #     print("\nla value : ", noeud.current_state[noeud.nb])
+            #     print("cost :", noeud.cost_value)
+            #     print("\netat avant tri\n", noeud.current_state)
+            
+            # self.opened = sorted(self.opened, key=lambda node: node.cost_value)
+
+
+            # for noeud in self.opened:
+            #     print("\n\nAPRES TRI\nla value : ", noeud.current_state[noeud.nb])
+            #     print("cost :", noeud.cost_value)
+            #     print("\netat apres tri\n", noeud.current_state)
             self.nb_states += 1
             self.max_nb_state = len(self.opened) if self.max_nb_state < len(self.opened) else self.max_nb_state
-            if self.opened[0].nb != dest:
-                del self.opened[0] # source d erreur potentiel
-            # print("dans le while a_star la liste opened :", self.opened)
-            # for nodee in self.opened:
-                # print("noeud ouvert :", nodee.current_state[nb[0]][nb[1]])
-            # print("dans le while a_star la liste closed :", self.opened)
-        target_noeud = self.opened[0]
-        ret = []
-        while target_noeud.parent_node != None:
-            ret.append(target_noeud)
-            target_noeud = target_noeud.parent_node
-        return ret
+ 
+            # if self.opened[0].nb != dest:
+            #     del self.opened[0] # source d erreur potentiel
 
-    def find_nb_to_move(self): # fonction a faire
-        return(1)
+        
+        print("nombre d iteration:", i)
+        print("nb chemin", len(self.opened))
+        # test = []
+        # for noeud in self.opened:
+        #     for tmp in self.opened:
+        #         if np.array_equal(noeud.current_state , tmp.current_state):
+        #             del tmp
+        i = 0
+        len_open = len(self.opened)
 
-    def algo_next_move_nb(self, nb, puzzle_base):
-        return format_where(np.where(puzzle_base == nb))
+        while i < len_open:
+            j = i + 1
+            while j < len_open:
+                if np.array_equal(self.opened[i].current_state , self.opened[j].current_state):
+                    del self.opened[j]
+                    print("\nDEL ELEMENT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+                    len_open -= 1
+                    j -= 1
+                j += 1
+            i += 1
 
-    def algo_find_target(self,nb, puzzle_target):
-        return format_where(np.where(puzzle_target == nb))
+        print("nb chemin apres les memes", len(self.opened))
+        
+
+        # print("\n END list open :")
+        # for noeud in self.opened:
+        #     print("\n\nAPRES TRI\nla value : ", noeud.current_state[noeud.nb])
+        #     print("cost :", noeud.cost_value)
+        #     print("\netat apres tri\n", noeud.current_state)
+
+
+        # print("\n END list closed :")
+
+        # for noeud in self.closed:
+        #     print("\n\nAPRES TRI\nla value : ", noeud.current_state[noeud.nb])
+        #     print("cost :", noeud.cost_value)
+        #     print("\netat apres tri\n", noeud.current_state)
+
+
+
+        print("debut etat \n")
+        tmp = self.closed[-1]
+        while tmp.parent_node != None:
+            print("Etat anterieur de l etat final :\n",tmp.current_state)
+            tmp = tmp.parent_node
+
+        print("\nfin des etats anterieur")
+        
+
+
+        print("last etat\n", self.closed[-1].current_state)
+        return self.closed[-1].zero_moves
+
+    def ft_verif(self, base, string):
+        for c in string:
+            if c == 't':
+                base = move_top(base, format_where(np.where(base == 0)))
+            if c == 'd':
+                base = move_bottom(base, format_where(np.where(base == 0)))
+            if c == 'r':
+                base = move_right(base, format_where(np.where(base == 0))) 
+            if c == 'l':
+                base = move_left(base, format_where(np.where(base == 0)))
+        return base
+                
+
 
 
     def algo(self, puzzle): # 1 itération du while = un placement de chiffre a la bonne place ( ex : le 1 en haut a gauche)
-        while not np.array_equal(self.current_state , puzzle.target)  and self.nb_move < 1: # pr test
-            nb_to_move = self.find_nb_to_move()
-            coord_nb_to_move = self.algo_next_move_nb(nb_to_move, self.current_state)
-            # print(coord_nb_to_move)
-            coord_dest = self.algo_find_target(nb_to_move, puzzle.target)
-            path = self.a_star(coord_nb_to_move, coord_dest)
-            print(path[0].current_state)
-            print("taille path", len(path))
-            while path[0].parent_node != None:
-                print(path[0].zero_moves)
-                path[0] = path[0].parent_node
-            exit()
-            for node in path:
-                self.nb_move += node.count_zero_moves
-            ft_moving(path) # on change le self.current_state
+        path = self.a_star(puzzle)
+        # print(puzzle.start)
+        print("path final:", path)
+        print("nombre de coups:", len(path))
+        print("puzzle start\n",puzzle.start)
+        print("puzzle final\n", puzzle.target)
+        print(self.ft_verif(puzzle.start, path))
+        exit()
+
+
 
